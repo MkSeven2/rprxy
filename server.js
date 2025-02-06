95bca20ebb031c0ff90ee569a1e3ce09dca1c9dd
MkSeven2 <sarsembayevdias7@gmail.com>
2025-02-05 20:56:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -65,9 +65,10 @@ function onProxyError(err, req, res) {
 }
 
 function onProxyReq(proxyReq, req, res, options) {
-  proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36');
-  proxyReq.removeHeader('roblox-id');
+  proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
+  proxyReq.removeHeader('roblox-id'); 
 }
+
 
 httpsProxy.on('error', onProxyError);
 httpsProxy.on('proxyReq', onProxyReq);
